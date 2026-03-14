7a7925282bdd62df8ad6fa0819b746f917f85491
Emilien.B <emilbus@outlook.fr>
2026-03-14 01:18:43 -0400

    Classe TestFilePrioriteLivraisons completer - Réponse question 1.10

--- MiniServer/src/main/java/com/test/TestFilePrioriteLivraisons.java
+++ MiniServer/src/main/java/com/test/TestFilePrioriteLivraisons.java
@@ -12,50 +12,89 @@ public class TestFilePrioriteLivraisons {
      * Test de la méthode estVide.
      */
     private static void testEstVide() {
+        FilePrioriteLivraisons file = new FilePrioriteLivraisons();
         System.out.print("Test estVide : ");
+        if(file.estVide()){
+            System.out.println("OK");
+        }else{
+            System.out.println("NOK");
+        }
 
         // TODO : À compléter/modifier
-        System.err.println("Méthode TestFilePrioriteLivraisons::testEstVide() non implémentée");
+        //em.err.println("Méthode TestFilePrioriteLivraisons::testEstVide() non implémentée");
     }
 
     /**
      * Test de la méthode ajouter.
      */
     private static void testAjouter() {
-        System.out.print("Test ajouter : ");
-
-        // TODO : À compléter/modifier
-        System.err.println("Méthode TestFilePrioriteLivraisons::testAjouter() non implémentée");
+        String result = "";
+        FilePrioriteLivraisons file = new FilePrioriteLivraisons();
+        Livraison FirstLivraison = new Livraison(Priorite.NORMALE, 1);
+        file.ajouter(FirstLivraison);
+        if(file.estVide()){
+            result = "NOK";
+        }else{
+            result = "OK";
+        }
+        System.out.println("Test ajouter : " + result);
     }
 
     /**
      * Test de la méthode ajouterTout.
      */
     private static void testAjouterTout() {
-        System.out.print("Test ajouterTout : ");
-
-        // TODO : À compléter/modifier
-        System.err.println("Méthode TestFilePrioriteLivraisons::testAjouterTout() non implémentée");
+        String result = "";
+        FilePrioriteLivraisons file = new FilePrioriteLivraisons();
+        Livraison FirstLivraison = new Livraison(Priorite.NORMALE, 1);
+        Livraison SecondLivraison = new Livraison(Priorite.URGENT, 1);
+        Livraison TreeLivraison = new Livraison(Priorite.NORMALE, 2);
+        Livraison ForLivraison = new Livraison(Priorite.URGENT, 2);
+        file.ajouterTout(Arrays.asList(FirstLivraison, SecondLivraison, TreeLivraison, ForLivraison));
+        
+        if(file.taille() == 4){
+            result = "OK";
+        }else{
+            result = "NOK";
+        }
+        System.out.print("Test ajouterTout : " + result);
     }
 
     /**
      * Test de la méthode taille.
      */
     private static void testTaille() {
-        System.out.print("Test taille : ");
-
-        // TODO : À compléter/modifier
-        System.err.println("Méthode TestFilePrioriteLivraisons::testTaille() non implémentée");
+        String result = "";
+        FilePrioriteLivraisons file = new FilePrioriteLivraisons();
+        Livraison FirstLivraison = new Livraison(Priorite.NORMALE, 1);
+        Livraison SecondLivraison = new Livraison(Priorite.URGENT, 1);
+        if(file.taille() == 0){
+            result = "OK";
+        }else{
+            result = "NOK";
+        }
+        System.out.print("Test taille : " + result);
     }
 
     /**
      * Test de la méthode retirer.
      */
     private static void testRetirer() {
-        System.out.print("Test retirer : ");
+        String result = "";
+        FilePrioriteLivraisons file = new FilePrioriteLivraisons();
+        Livraison FirstLivraison = new Livraison(Priorite.NORMALE, 1);
+        Livraison SecondLivraison = new Livraison(Priorite.URGENT, 1);
+        file.ajouterTout(Arrays.asList(FirstLivraison, SecondLivraison));
+        file.retirer();
+        if(file.taille() == 1){
+            result = "OK";
+        }else{
+            result = "NOK";
+        }
+        System.out.print("Test retirer : " + result);
 
         // TODO : À compléter/modifier
-        System.err.println("Méthode TestFilePrioriteLivraisons::testRetirer() non implémentée");
+        //System.err.println("Méthode TestFilePrioriteLivraisons::testRetirer() non implémentée");
     }
 
     /**
